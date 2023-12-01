d632ae2552a35d0200b4c116dc359eda2abf041d
Lars-Erik Aabech <lea@markedspartner.no>
2023-12-01 15:24:16 +0100

    Create fail-part-2.ps1

---
+++ day1/fail-part-2.ps1
@@ -0,0 +1,1 @@
+$pairs = ("one", "1"), ("two", "2"), ("three", "3"), ("four", "4"), ("five", "5"), ("six", "6"), ("seven", "7"), ("eight", "8"), ("nine", "9"), ("[^\d]", ""); (Get-Clipboard).Split('\n') | % { $line = $_; ($pairs | % { $line = $line -replace $_[0], $_[1] }); $line } | % { $_[0] + $_[$_.length - 1] } | % { [System.Convert]::ToInt32($_) }
